f6239ef861475b65a750358b117cfe12019ccb18
Victor <victornuneshvh10@gmail.com>
2024-01-30 15:16:16 -0300

    successfully

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -2,16 +2,15 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { user, password, database } = process.env;
-
 // const conString = "postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName";
+console.log(process.env);
 
 const client = new pg.Pool({
     host: 'db', 
     port: 5432,
-    user: user, 
-    password: password, 
-    database: database
+    user: process.env.POSTGRES_USER, 
+    password: process.env.POSTGRES_PASSWORD, 
+    database: process.env.POSTGRES_DB
 });
 
 export default client;
